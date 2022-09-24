c451d0c8a0c8772ad4c29d6a292c80001dcafe95
Diego Faria <diego.rdfaria@gmail.com>
2022-09-24 13:19:49 -0300

    Rename route to remove duplicated api route name

--- Api/Controllers/ApiController.cs
+++ Api/Controllers/ApiController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ApiController : ControllerBase
     {

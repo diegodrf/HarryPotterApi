41304aa0df1c593f1268f75843a6a5122599f7fb
Diego Faria <diego.rdfaria@gmail.com>
2022-10-16 17:49:39 -0300

    Add Repository tests

--- HarryPotterApi.Tests/Repositories/HousesRepositoryTests.cs
+++ HarryPotterApi.Tests/Repositories/HousesRepositoryTests.cs
@@ -1,5 +1,6 @@
 ﻿using HarryPotterApi.Constants;
 using HarryPotterApi.Data.Connections;
+using HarryPotterApi.Models.Data;
 using HarryPotterApi.Repositories;
 using HarryPotterApi.Services;
 using Microsoft.EntityFrameworkCore;
@@ -11,24 +12,23 @@ namespace HarryPotterApi.Tests.Repositories
     {
         private readonly Uri _imagesBaseUrl;
         private readonly Uri _charactersDataSource;
-        private readonly HarryPotterApiDbContext _dbContext;
+        private HarryPotterApiDbContext? _dbContext;
 
 
         public HousesRepositoryTests()
         {
             _imagesBaseUrl = new Uri("https://raw.githubusercontent.com/diegodrf/HarryPotterApi/main/Assets/Images/");
             _charactersDataSource = new Uri("https://raw.githubusercontent.com/diegodrf/HarryPotterApi/main/Assets/characters.json");
-
-            var options = new DbContextOptionsBuilder<HarryPotterApiDbContext>()
-                .UseInMemoryDatabase(Guid.NewGuid().ToString())
-                .Options;
-            _dbContext = new HarryPotterApiDbContext(options);
-            
         }
 
         [TestInitialize]
         public async Task TearUp()
         {
+            var options = new DbContextOptionsBuilder<HarryPotterApiDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            _dbContext = new HarryPotterApiDbContext(options);
+
             var dataSeeding = new DataSeedingService(_dbContext, _imagesBaseUrl, _charactersDataSource);
             await dataSeeding.Run();
         }
@@ -41,14 +41,51 @@ namespace HarryPotterApi.Tests.Repositories
 
 
         [TestMethod]
-        public async Task TestMethod()
+        public async Task Should_Return_4_Houses()
         {
-
-            var houseRepository = new HousesRepository(_dbContext);
+            var houseRepository = new HousesRepository(_dbContext!);
 
             var _ = await houseRepository.GetAllAsync(0, 10);
             Assert.AreEqual(4, _.Count());
+        }
 
+        [TestMethod]
+        public async Task Should_Return_Characters_From_House()
+        {
+            var houseRepository = new HousesRepository(_dbContext!);
+
+            var characters = await houseRepository.GetCharactersAsync(1, 0, 25);
+            
+            Assert.AreEqual(typeof(List<Character>), characters.GetType());
+            Assert.IsTrue(characters.Count > 0);
+        }
+
+        [TestMethod]
+        public async Task Should_Return_Total_Of_Houses()
+        {
+            var houseRepository = new HousesRepository(_dbContext!);
+
+            var housesQuantity = await houseRepository.GetAllCountAsync();
+            Assert.IsTrue(housesQuantity > 0);
+        }
+
+        [TestMethod]
+        public async Task Should_Return_Total_Of_Characters_From_Houses()
+        {
+            var houseRepository = new HousesRepository(_dbContext!);
+
+            var charactersQuantity = await houseRepository.GetCharactersCountAsync(1);
+            Assert.IsTrue(charactersQuantity > 0);
+        }
+
+        [TestMethod]
+        public async Task Should_Return_Zero_Characters_From_Houses()
+        {
+            var houseRepository = new HousesRepository(_dbContext!);
+
+            var characters = await houseRepository.GetCharactersAsync(1000, 0, 100);
+            Assert.AreEqual(typeof(List<Character>), characters.GetType());
+            Assert.IsTrue(characters.Count == 0);
         }
     }
 }

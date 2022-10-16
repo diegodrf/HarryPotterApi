e77fc71adbb040b1769e2c415715ab0919d318a7
Diego Faria <diego.rdfaria@gmail.com>
2022-10-16 16:12:09 -0300

    Create swagger dependency injection extension to organize project

---
+++ HarryPotterApi/DependencyIntjections/SwaggerDepentendyInjectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace HarryPotterApi.DependencyIntjections
+{
+    public static class SwaggerDepentendyInjectionExtensions
+    {
+        public static void AddSwaggerConfigurations(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(configurations =>
+            {
+                configurations.EnableAnnotations();
+                configurations.SwaggerDoc(
+                    "v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Harry Potter API",
+                        Version = "v1",
+                        License = new OpenApiLicense { Name = "MIT License", Url = new Uri("https://github.com/diegodrf/HarryPotterApi/blob/main/LICENSE.md") },
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Diego Faria",
+                            Url = new Uri("https://github.com/diegodrf")
+                        }
+                    });
+            });
+        }
+    }
+}

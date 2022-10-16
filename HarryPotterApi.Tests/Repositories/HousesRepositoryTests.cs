7cdaff49e00fba4b4108f478d4180a5b0bfb9c6b
Diego Faria <diego.rdfaria@gmail.com>
2022-10-16 17:24:25 -0300

    Refactor integration test

--- HarryPotterApi.Tests/Repositories/HousesRepositoryTests.cs
+++ HarryPotterApi.Tests/Repositories/HousesRepositoryTests.cs
@@ -1,41 +1,53 @@
 ﻿using HarryPotterApi.Data.Connections;
-using HarryPotterApi.Models.Data;
 using HarryPotterApi.Repositories;
+using HarryPotterApi.Services;
 using Microsoft.EntityFrameworkCore;
-using Moq;
 
 namespace HarryPotterApi.Tests.Repositories
 {
     [TestClass]
     public class HousesRepositoryTests
     {
+        private readonly Uri _imagesBaseUrl;
+        private readonly Uri _charactersDataSource;
+        private readonly HarryPotterApiDbContext _dbContext;
+
+
+        public HousesRepositoryTests()
+        {
+            _imagesBaseUrl = new Uri("https://raw.githubusercontent.com/diegodrf/HarryPotterApi/main/Assets/Images/");
+            _charactersDataSource = new Uri("https://github.com/diegodrf/HarryPotterApi/main/Assets/characters.json");
+
+            var options = new DbContextOptionsBuilder<HarryPotterApiDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            _dbContext = new HarryPotterApiDbContext(options);
+            
+        }
+
+        [TestInitialize]
+        public async Task TearUp()
+        {
+            var dataSeeding = new DataSeedingService(_dbContext, _imagesBaseUrl, _charactersDataSource);
+            await dataSeeding.Run();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+
+        }
+
+
         [TestMethod]
         public async Task TestMethod()
         {
-            var houses = new List<House>()
-            {
-                new House() {Id = 1, Name ="Gryffindor"},
-                new House() {Id = 2, Name ="Slytherin"},
-                new House() {Id = 3, Name ="Hufflepuff"},
-                new House() {Id = 4, Name ="Ravenclaw"}
-            };
-            var dbSetMock = new Mock<DbSet<House>>();
-            //dbSetMock.Object.AddRange(houses);
-            dbSetMock.As<IQueryable<House>>().Setup(m => m.Provider).Returns(houses.AsQueryable().Provider);
-            dbSetMock.As<IQueryable<House>>().Setup(m => m.Expression).Returns(houses.AsQueryable().Expression);
-            dbSetMock.As<IQueryable<House>>().Setup(m => m.ElementType).Returns(houses.AsQueryable().ElementType);
-            dbSetMock.As<IQueryable<House>>().Setup(m => m.GetEnumerator()).Returns(houses.AsQueryable().GetEnumerator());
 
-
-            var dbContextMock = new Mock<HarryPotterApiDbContext>(new DbContextOptions<HarryPotterApiDbContext>());
-            dbContextMock.Object.Houses.AddRange(houses);
-            
-           
-            var houseRepository = new HousesRepository(dbContextMock.Object);
+            var houseRepository = new HousesRepository(_dbContext);
 
             var _ = await houseRepository.GetAllAsync(0, 10);
             Assert.AreEqual(4, _.Count());
-            
+
         }
     }
 }
